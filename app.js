16618f71541221d2d2a87f908e2b9bae5bab45a6
mila-hristova <eaa23mihr@students.eaaa.dk>
2024-05-28 09:58:26 +0200

    content added

--- app.js
+++ app.js
@@ -3,6 +3,34 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const posts = await getPosts(); //Call the getPosts function
+  posts.sort((a, b) => new Date (a.Date).getTime() - new Date(b.date).getTime())
+  console.log (posts); //Log the posts to the console
+  displayPostsGrid (posts); //Call the displayPosts function with the posts 
+}
+
+async function getPosts () {
+  const response = await fetch(
+    "https://2sem.programming.mila-hristova.com/wp-json/wp/v2/posts?acf_format=standard"
+  );
+  const data = await response.json();
+  return data;
+}
+
+function displayPostsGrid(posts) {
+  const postsGrid = document.querySelector ("#posts-grid");
+
+  for (const post of posts) {
+    postsGrid.insertAdjacentHTML(
+      "beforeend",
+      `
+      <article class="grid-item">
+        <img src="${post.acf.add_image}" alt="${post.title.rendered}" />
+        <h2>${post.title.rendered} </h2>
+      </article>
+      `
+    )
+  }
 }
